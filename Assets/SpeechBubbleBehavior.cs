d7a79e6e92da6a50c6d55072d833326ca91714bf
David <dsanosa@gmail.com>
2019-03-05 18:46:11 -0800

    added teleportation, started to add keyboard

---
+++ Assets/SpeechBubbleBehavior.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubbleBehavior : MonoBehaviour
+{
+    public int foobar;
+    public TMPro.TextMeshPro textObj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        textObj.text = "hey";
+    }
+}
